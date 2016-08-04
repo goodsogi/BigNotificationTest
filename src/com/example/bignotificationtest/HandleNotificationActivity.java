94e58e02cfb9b0469198fd7ae440225c31fc6e60
Jeonggyu <goodsogi@gmail.com>
2016-08-04 10:57:20 +0900

    first commit

---
+++ src/com/example/bignotificationtest/HandleNotificationActivity.java
@@ -0,0 +1,13 @@
+package com.example.bignotificationtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+ 
+public class HandleNotificationActivity extends Activity {
+ 
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.handle_notification_activity);
+ }
+}
